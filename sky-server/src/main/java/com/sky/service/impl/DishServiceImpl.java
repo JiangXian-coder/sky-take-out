9f92142c02460cf2eb436e8876f82346eb2a647a
JiangXian-coder <2950834262@qq.com>
2025-12-17 15:38:45 +0800

    菜品的新增与上传图片代码开发(完毕,1.0)

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,46 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    /**
+     * 保存菜品及其口味信息
+     * @param dishDTO 菜品传输对象，包含菜品基本信息和口味列表
+     */
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+        //插入菜品数据
+        dishMapper.insert(dish);
+        //获取插入的菜品的id
+        Long dishId = dish.getId();
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+
+        //批量插入口味数据
+        if (flavors!=null && flavors.size()>0){
+            flavors.forEach(dishFlavor -> dishFlavor.setDishId(dishId));
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+
+}
